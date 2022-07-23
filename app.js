728e368b886967d347012e60a17f22762f199aa1
Emmanuel Zelarayan <emmavz@live.com>
2022-07-23 18:12:18 -0300

    error handlers and middlewares

--- app.js
+++ app.js
@@ -3,25 +3,19 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
-// middleware
-
+// middlewares
+app.use(express.static("./public"));
 app.use(express.json());
 
 // routes
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
-
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-// app.get("/api/v1/tasks")         - get all the tasks
-// app.post("/api/v1/tasks")        - create a new task
-// app.get("/api/v1/tasks/:id")     - get single task
-// app.patch("/api/v1/tasks/:id")   - update task
-// app.delete("/api/v1/tasks/:id")  - delete task
-
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
